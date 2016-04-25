f528dd82e538f899d99f426b3e4d3540bfdb4125
Michaelangel007 <testcraft@sharedcraft.com>
2016-04-25 01:57:39 -0700

    Add int64_t

--- source/util_types.h
+++ source/util_types.h
@@ -2,3 +2,5 @@ typedef unsigned char      uint8_t;
 typedef unsigned short     uint16_t;
 typedef unsigned int       uint32_t;
 typedef unsigned long long uint64_t;
+
+typedef signed   long long int64_t;
