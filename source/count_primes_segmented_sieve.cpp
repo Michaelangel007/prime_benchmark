126a0d5e8e899af93ad672495c1ace63fee735d5
Michaelangel007 <testcraft@sharedcraft.com>
2016-04-28 10:37:56 -0700

    Added largest prime to segmented sieve

--- source/count_primes_segmented_sieve.cpp
+++ source/count_primes_segmented_sieve.cpp
@@ -15,6 +15,7 @@
 #include <stdint.h>
 
 #include "util_timer.h"
+uint64_t gnLargestPrime = 0;
 
 /// Set your CPU's L1 data cache size (in bytes) here
 const int L1D_CACHE_SIZE = 32768;
@@ -39,13 +40,13 @@ int segmented_sieve(int64_t limit, int segment_size = L1D_CACHE_SIZE)
   std::vector<char> is_prime(sqrt + 1, 1);
 
 #if DEBUG // BEGIN --- DEBUG
-int64_t largest = 0;
-// is_prime[] is filled with 1's
-printf( "sqrt: %u\n", sqrt );
-printf( "is_prime.length = %zu\n", is_prime.size() );
-for( int z = 0; z < 16; z++ )
-    printf( "%d, ", is_prime[z] ); // 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
-printf( "\n" );
+    int64_t largest = 0;
+    // is_prime[] is filled with 1's
+    printf( "sqrt: %u\n", sqrt );
+    printf( "is_prime.length = %zu\n", is_prime.size() );
+    for( int z = 0; z < 16; z++ )
+        printf( "%d, ", is_prime[z] ); // 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+    printf( "\n" );
 #endif // END --- DEBUG
 
   for (int i = 2; i * i <= sqrt; i++)
@@ -54,9 +55,9 @@ printf( "\n" );
         is_prime[j] = 0;
 
 #if DEBUG // BEGIN --- DEBUG
-for( int z = 0; z < 16; z++ )      // 0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15
-    printf( "%d, ", is_prime[z] ); // 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0,
-printf( "\n" );
+    for( int z = 0; z < 16; z++ )      // 0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15
+        printf( "%d, ", is_prime[z] ); // 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0,
+    printf( "\n" );
 #endif // END --- DEBUG
 
   std::vector<int> primes;
@@ -78,8 +79,9 @@ printf( "\n" );
           next.push_back((int)(s * s - low));
       }
     }
+
     // sieve the current segment
-    for (std::size_t i = 1; i < primes.size(); i++)
+    for (size_t i = 1; i < primes.size(); i++)
     {
       int j = next[i];
       for (int k = primes[i] * 2; j < segment_size; j += k)
@@ -89,11 +91,16 @@ printf( "\n" );
 
     for (; n <= high; n += 2)
       if (sieve[n - low]) // n is a prime
+      {
         count++;
+        gnLargestPrime = n + low;
+    }
   }
 
+// begin patch
 //  std::cout << count << " primes found." << std::endl;
     return count;
+// end patch
 }
 
 /// Usage: ./segmented_sieve n size
@@ -103,7 +110,7 @@ printf( "\n" );
 int main(int argc, char** argv)
 {
   // generate the primes below this number
-  int64_t limit = 100000000;
+  int64_t limit = 10000000; // 10^7
   if (argc >= 2)
     limit = atol(argv[1]);
 
@@ -111,14 +118,17 @@ int main(int argc, char** argv)
   if (argc >= 3)
     size = atoi(argv[2]);
 
+// begin patch
 Timer timer;
 timer.Start();
   int primes = segmented_sieve(limit, size);
 timer.Stop();
 timer.Throughput( limit );
 
-printf( "Primes = %u\n", primes );
+printf( "Primes : %u\n", primes );
+printf( "Largest: %llu\n", gnLargestPrime );
 printf( "Elapsed: %f %s\n", timer.elapsed, timer.data.hms );
+// end patch
 
   return 0;
 }
